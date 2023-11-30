dfe2ce4a7d4322fa493f001166f6032275847cfe
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-30 13:28:56 +0300

    tasks done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import com.workintech.models.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1,"Kerem","Karaman"));
+        employees.add(new Employee(1,"Kerem","Karaman"));
+        employees.add(new Employee(2,"joe","doe"));
+        employees.add(new Employee(2,"king","kingsman"));
+        employees.add(new Employee(3,"queen","kingsman"));
+        employees.add(new Employee(3,"queen2","queen2"));
+        employees.add(new Employee(4,"ali","cs"));
+        employees.add(new Employee(5,"mac","cs-"));
+
+        Map<Integer,Employee> employeeMap = new HashMap<>();
+        List<Employee> removedEmployees = new ArrayList<>();
+
+        Iterator<Employee>iterator=employees.iterator();
+        while (iterator.hasNext()){
+            Employee employee =iterator.next();
+            if(employee==null){
+                System.out.println("There is null data");
+                continue;
+            }if (employeeMap.containsKey(employee.getId())){
+                removedEmployees.add(employee);
+                iterator.remove();
+            }else {
+                employeeMap.put(employee.getId(),employee);
+            }
+        }
+        System.out.println("mevcut:");
+        System.out.println(employees);
+        System.out.println("tekiller: ");
+        System.out.println(employeeMap);
+        System.out.println("silinenler: ");
+        System.out.println(removedEmployees);
+
     }
 }
---
+++ src/com/workintech/models/Employee.java
@@ -0,0 +1,61 @@
+package com.workintech.models;
+
+import java.util.Objects;
+
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
